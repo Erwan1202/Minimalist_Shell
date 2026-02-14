5349062f65dc8777b52a0818cb446f8d2da3bdb8
erwan <erwan.a.marechal@gmail.com>
2026-02-14 20:56:27 +0100

    Déclaration du tempon pour le fgets, prompt accueil utilisateur, fflush pour vider le tampon. A faire : fgets

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void){
+	char buffer_fgets [1024];
+
+	while(1){
+		printf("MiniShell >>");
+		fflush(stdout);
+
+}
