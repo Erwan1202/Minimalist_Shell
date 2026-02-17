c98e78bec843899ba07f134ef8dae2998f5c3de6
erwan <erwan.a.marechal@gmail.com>
2026-02-17 10:10:45 +0100

    Amélioration de la lecture de l'entrée utilisateur avec fgets, ajout de la vérification de la chaîne vide et gestion des commandes "exit" et "history".

--- src/main.c
+++ src/main.c
@@ -9,11 +9,32 @@ int main(void){
 	while(1){
 		printf("MiniShell >>");
 		fflush(stdout);
-	
-		fgets(buffer_fgets, 1024, stdin);
-		//printf("You entered: %s", buffer_fgets, "\n");
+		// Read user input using fgets
+		// Vérification du fgets : la lecture est elle réussi (si non NULL on exit la boucle)
+		if (fgets(buffer_fgets, sizeof(buffer_fgets), stdin)!= NULL){
 
+			// Nettoyage du "\n"
+			buffer_fgets[strcspn(buffer_fgets, "\n")] = 0; // strcspn : retourne la position du premier caractère de "\n" dans buffer_fgets, et on le remplace par '\0' pour terminer la chaîne de caractères proprement.
+			
+			// Vérification de la chaine vide
+			if (strlen(buffer_fgets) == 0){
+				continue; 
+			}
+
+			// Vérification de la commande "exit"
+			if (strcmp(buffer_fgets, "exit") == 0){
+				printf("Exiting MiniShell...\n");
+				break;
+			
+			if (strcmp(buffer_fgets, "history") == 0){
+				printf("Command history is not implemented yet.\n");
+				continue;
+			}
+		};
+		//printf("You entered: %s", buffer_fgets, "\n");
 		
+
+		// History command
 
 
 	}
